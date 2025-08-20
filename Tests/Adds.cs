62cbb51109902ad5a6922987369c3e93150ffb04
Rajesh T <100931794+rthirumalasetti@users.noreply.github.com>
2025-08-20 18:56:24 +0530

    Create Adds.cs

---
+++ Tests/Adds.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SampleApps
+{
+    class programs
+    {
+        static void Main(string[] args)
+        {
+            var Result = AddNumbers(5, 10);
+            Console.WriteLine("Result: " + Result);
+
+            var list = new List<string>();
+            list.Add("Item1");
+            list.Add("Item2");
+            list.Add("Item3");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            DoNothing();
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        // public static int AddTwoNumbers(int a, int b)
+        // {
+        //     return a + b;
+        // }
+
+
+        static void DoNothing()
+        {
